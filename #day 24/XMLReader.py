daeb5b74916828287d0b9c173e6b52aa88967541
Harsh kushwaha <harsh90064@gmail.com>
2025-11-03 19:24:44 +0530

    #day 24 complete streak 24/100

---
+++ #day 24/XMLReader.py
@@ -0,0 +1,13 @@
+import xml.etree.ElementTree as ET
+
+tree=ET.parse('employee.xml')
+root = tree.getroot()
+
+print("Name | Age | Gender | Phone | Address")
+for emp in root.findall('employee'):
+    name=emp.find('name').text
+    age=emp.find('age').text
+    gender=emp.find('gender').text
+    phone=emp.find('phone').text
+    address=emp.find('address').text
+    print('{} | {} | {} | {} | {} '.format(name,age,gender,phone,address))
